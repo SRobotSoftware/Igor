c05b2f7e76cebff76d40385c0828ebbbed1d130f
Zmixon91 <zmixon91@gmail.com>
2016-02-17 17:22:04 -0700

    updates small things

--- www/app/config/routes.js
+++ www/app/config/routes.js
@@ -1,13 +1,17 @@
 app.config(function ($stateProvider, $urlRouterProvider) {
-      $urlRouterProvider.otherwise('/home');
+      $urlRouterProvider.otherwise('/login');
       
       $stateProvider
-        .state('home', {
-          url: '/home',
-          templateUrl: 'app/components/home/home.html'
+        .state('login', {
+          url: '/login',
+          templateUrl: 'app/components/login/login.html'
         })
         .state('about', {
           url: '/about',
           templateUrl: 'app/components/about/about.html'
+        })
+        .state('dashboard', {
+          url: '/dashboard',
+          tempalteUrl: 'app/components/dashboard/dashboard.html'
         })
   });
