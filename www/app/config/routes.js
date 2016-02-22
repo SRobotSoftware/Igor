99b9c3fa11f5007d7da5aaa6c020c66268bebf14
Zmixon91 <zmixon91@gmail.com>
2016-02-22 13:39:36 -0700

    breaks everything

--- www/app/config/routes.js
+++ www/app/config/routes.js
@@ -1,17 +1,21 @@
 app.config(function ($stateProvider, $urlRouterProvider) {
-      $urlRouterProvider.otherwise('/login');
-      
-      $stateProvider
-        .state('login', {
-          url: '/login',
-          templateUrl: 'app/components/login/login.html'
-        })
-        .state('about', {
-          url: '/about',
-          templateUrl: 'app/components/about/about.html'
-        })
-        .state('dashboard', {
-          url: '/dashboard',
-          tempalteUrl: 'app/components/dashboard/dashboard.html'
-        })
-  });
+	$urlRouterProvider.otherwise('/login');
+
+	$stateProvider
+		.state('login', {
+			url: '/login',
+			templateUrl: 'app/components/login/login.html'
+		})
+		.state('register', {
+			url: '/register',
+			templateUrl: 'app/components/register/register.html'
+		})
+		.state('dashboard', {
+			url: '/dashboard',
+			tempalteUrl: 'app/components/dashboard/dashboard.html'
+		})
+		.state('classroom', {
+			url: '/classroom', // Pull uid from URL
+			templateUrl: 'app/components/classroom/classroom.html'
+		})
+});
