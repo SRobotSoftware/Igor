dadf5cd775d37946799964d14d2aa3a025f509b1
Zmixon91 <zmixon91@gmail.com>
2016-02-24 17:13:22 -0700

    some functions work kinda

--- www/app/config/init.js
+++ www/app/config/init.js
@@ -5,60 +5,145 @@
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('starter', ['ionic', 'ui.router', 'js-data', 'firebase'])
 
-app.config(function (DSFirebaseAdapterProvider) {
-    DSFirebaseAdapterProvider.defaults.basePath = 'https://discoapp.firebaseio.com';
+app.config(function (DSFirebaseAdapterProvider, DSProvider) {
+	DSFirebaseAdapterProvider.defaults.basePath = 'https://discoapp.firebaseio.com';
+
+	angular.extend(DSProvider.defaults, {
+		beforeCreate: function (resource, data, cb) {
+			if (resource.defaultAdapter === 'firebase') {
+				data = fixKeys(data, true);
+			}
+			cb(null, data);
+		},
+		beforeUpdate: function (resource, data, cb) {
+			if (resource.defaultAdapter === 'firebase') {
+				data = fixKeys(data, true);
+			}
+			cb(null, data);
+		},
+		afterCreate: function (resource, data, cb) {
+			if (resource.defaultAdapter === 'firebase') {
+				data = fixKeys(data, false);
+			}
+			cb(null, data);
+		},
+		afterUpdate: function (resource, data, cb) {
+			if (resource.defaultAdapter === 'firebase') {
+				data = fixKeys(data, false);
+			}
+			cb(null, data);
+		},
+		afterFind: function (resource, data, cb) {
+			if (resource.defaultAdapter === 'firebase') {
+				data = fixKeys(data, false);
+			}
+			cb(null, data);
+		},
+		afterFindAll: function (resource, data, cb) {
+			if (resource.defaultAdapter === 'firebase') {
+				if (!Array.isArray(data)) {
+					data = fixKeys(data, false);
+				} else {
+					data = data.map(function (d) {
+						return fixKeys(d, false);
+					})
+				}
+			}
+			cb(null, data);
+		},
+		});
 });
 
 app.run(function ($ionicPlatform, $rootScope, DS, DSFirebaseAdapter, User) {
-    $ionicPlatform.ready(function () {
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	$ionicPlatform.ready(function () {
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+			// for form inputs)
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-            // Don't remove this line unless you know what you are doing. It stops the viewport
-            // from snapping when text inputs are focused. Ionic handles this internally for
-            // a much nicer keyboard experience.
-            cordova.plugins.Keyboard.disableScroll(true);
-        }
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-    });
+			// Don't remove this line unless you know what you are doing. It stops the viewport
+			// from snapping when text inputs are focused. Ionic handles this internally for
+			// a much nicer keyboard experience.
+			cordova.plugins.Keyboard.disableScroll(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
 
-    // 	$rootScope.$on('$stateChangeStart', function (event, toState, toStateParams) {
-    // 	AuthService.authMember();
-    // });
+	//DS.adapters.firebase === DSFirebaseAdapter;
+	angular.forEach(DS.definitions, function (Resource) {
+				if (Resource.defaultAdapter !== 'firebase') return;
+				var ref = DSFirebaseAdapter.ref.child(Resource.endpoint);
+				// Inject items into the store when they're added to Firebase
+				// Update items in the store when they're modified in Firebase
+				ref.on('child_changed', function (dataSnapshot) {
+			var data = dataSnapshot.val();
+			if (data[Resource.idAttribute]) {
+				Resource.inject(fixKeys(data));
+			}
+				});
+				// Eject items from the store when they're removed from Firebase
+				ref.on('child_removed', function (dataSnapshot) {
+			var data = dataSnapshot.val();
+			if (data[Resource.idAttribute]) {
+				Resource.eject(data[Resource.idAttribute]);
+			}
+				});
+	});
 
-    DS.registerAdapter('firebase',
-        DSFirebaseAdapter,
-        { default: true });
-    angular.forEach(DS.definitions, function (Resource) {
-        var ref = DSFirebaseAdapter.ref.child(Resource.endpoint);
-        ref.on('child_changed', function (dataSnapshot) {
-            var data = dataSnapshot.val();
-            if (data[Resource.idAttribute]) {
-                Resource.inject(data);
-            }
-        });
-        ref.on('child_removed', function (dataSnapshot) {
-            var data = dataSnapshot.val();
-            if (data[Resource.idAttribute]) {
-                Resource.eject(data[Resource.idAttribute]);
-            }
-        })
-    });
+	// 	$rootScope.$on('$stateChangeStart', function (event, toState, toStateParams) {
+	// 	AuthService.authMember();
+	// });
 
+	DS.registerAdapter('firebase',
+		DSFirebaseAdapter,
+		{ default: true });
 });
 
 app.factory('User', function (DS) {
-    return DS.defineResource('users');
+	return DS.defineResource({
+		name: 'users'
+		});
 });
 app.factory('Classroom', function (DS) {
-    return DS.defineResource({
-        name: 'classroom',
-        hasMany: {
-            users: 'users'
-        }
-    });
+	return DS.defineResource({
+		name: 'classrooms'
+	});
 });
+
+function fixKeys(obj, encode) {
+		if (!Array.isArray(obj) && typeof obj != 'object') return obj;
+		return Object.keys(obj).reduce(function (acc, key) {
+			var fixedKey = key;
+			if (encode) {
+				fixedKey = encodeAsFirebaseKey(fixedKey);
+			} else {
+				fixedKey = decodeFirebaseKey(fixedKey);
+			}
+			acc[fixedKey] = fixKeys(obj[key], encode);
+			return acc;
+		}, Array.isArray(obj) ? [] : {});
+	}
+
+	function encodeAsFirebaseKey(val) {
+		return val
+			.replace(/\%/g, '%25')
+			.replace(/\./g, '%2E')
+			.replace(/\#/g, '%23')
+			.replace(/\$/g, '%24')
+			.replace(/\//g, '%2F')
+			.replace(/\[/g, '%5B')
+			.replace(/\]/g, '%5D');
+	};
+
+	function decodeFirebaseKey(val) {
+		return val
+			.replace(/\%25/g, '%')
+			.replace(/\%2E/g, '.')
+			.replace(/\%23/g, '#')
+			.replace(/\%24/g, '$')
+			.replace(/\%2F/g, '/')
+			.replace(/\%5D/g, ']')
+			.replace(/\%5B/g, '[')
+	};
