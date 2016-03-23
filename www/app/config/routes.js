2d88581526513e966a563cddad4643285903e706
McSkwid <zmixon91@gmail.com>
2016-03-22 21:41:25 -0600

    Removal of register template, not used

--- www/app/config/routes.js
+++ www/app/config/routes.js
@@ -1,29 +1,25 @@
 angular
 	.module('Disco')
 	.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/login');
+		$urlRouterProvider.otherwise('/login');
 
-	$stateProvider
-		.state('login', {
-			url: '/login',
-			templateUrl: 'app/components/login/login.html',
-			controller: 'LoginController',
-			controllerAs: 'lc'
-		})
-		.state('register', {
-			url: '/register',
-			templateUrl: 'app/components/register/register.html'
-		})
-		.state('dashboard', {
-			url: '/dashboard',
-			templateUrl: 'app/components/dashboard/dashboard.html',
-			controller: 'DashboardController',
-			controllerAs: 'dc'
-		})
-		.state('classroom', {
-			url: '/dashboard/:classroomId',
-			templateUrl: 'app/components/classroom/classroom.html',
-			controller: 'ClassroomController',
-			controllerAs: 'cc'
-		});
-});
+		$stateProvider
+			.state('login', {
+				url: '/login',
+				templateUrl: 'app/components/login/login.html',
+				controller: 'LoginController',
+				controllerAs: 'lc'
+			})
+			.state('dashboard', {
+				url: '/dashboard',
+				templateUrl: 'app/components/dashboard/dashboard.html',
+				controller: 'DashboardController',
+				controllerAs: 'dc'
+			})
+			.state('classroom', {
+				url: '/dashboard/:classroomId',
+				templateUrl: 'app/components/classroom/classroom.html',
+				controller: 'ClassroomController',
+				controllerAs: 'cc'
+			});
+	});
